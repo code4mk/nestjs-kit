324a7f23787df46e6eb813190ed303b6ade26253
Mostafa Kamal <hiremostafa@gmail.com>
2023-01-19 22:13:07 +0600

    standard response object

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'no-constructor-return': 0,
     'no-unreachable': 0,
     'consistent-return': 0,
+    'no-shadow': 0,
 
     // eslint-config-import rules.
     'import/prefer-default-export': 0,
